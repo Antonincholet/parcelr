b2767ad1ae890f54284a8cca90be474415d13c0a
antonin cholet <antonin@cholet-avocat.fr>
2020-06-10 10:51:52 +0200

    add route map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -27,48 +27,12 @@ import "bootstrap";
 
 // Internal imports, e.g:
 // import { initSelect2 } from '../components/init_select2';
-import * as Gp from "../pluggins/GpSDK2D";
+import { map } from '../pluggins/map'
 
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
-  var map = Gp.Map.load(
-      "map", // html div
-      {
-           // Geoportal access key obtained here : http://professionnels.ign.fr/ign/contrats
-           apiKey: "choisirgeoportail",
-           // map center
-           center : {
-               location : "73 avenue de Paris, Saint-Mandé"
-           },
-           style : 'bdparcellaire',
-           // map zoom level
-           zoom : 18,
-           // layers to display
-           layersOptions : {
-               "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD" : {
-                opacity : 0.2
-               },
-               "CADASTRALPARCELS.PARCELS" : {
-                   opacity : 0.7
-               }
-           },
-           // additional tools to display on the map
-           controlsOptions : {
-               "search" : {
-                   maximised : true
-               }
+  map();
 
-           },
-           // markers to put in the map
-           markersOptions : [{
-               content : "<h1>Pôle Géosciences</h1><br/><p>73 avenue de Paris, Saint-Mandé</p><br/><p><a href='http://www.pôle-géosciences.fr/index.htm' target='_blank'>Site Web</a></p>"
-           }]
-      }
-  ) ;
-
-
-  var infoDiv = document.getElementById("info");
-  infoDiv.innerHTML = "<p> SDK version " + Gp.sdkVersion + " (" + Gp.sdkDate + ")</p>";
 
 });
---
+++ app/javascript/pluggins/map.js
@@ -0,0 +1,42 @@
+import * as Gp from "../pluggins/GpSDK2D";
+
+var map = Gp.Map.load(
+      "map", // html div
+      {
+           // Geoportal access key obtained here : http://professionnels.ign.fr/ign/contrats
+           apiKey: "choisirgeoportail",
+           // map center
+           center : {
+               location : "73 avenue de Paris, Saint-Mandé"
+           },
+           style : 'bdparcellaire',
+           // map zoom level
+           zoom : 18,
+           // layers to display
+           layersOptions : {
+               "GEOGRAPHICALGRIDSYSTEMS.MAPS.SCAN-EXPRESS.STANDARD" : {
+                opacity : 0.2
+               },
+               "CADASTRALPARCELS.PARCELS" : {
+                   opacity : 0.7
+               }
+           },
+           // additional tools to display on the map
+           controlsOptions : {
+               "search" : {
+                   maximised : true
+               }
+
+           },
+           // markers to put in the map
+           markersOptions : [{
+               content : "<h1>Pôle Géosciences</h1><br/><p>73 avenue de Paris, Saint-Mandé</p><br/><p><a href='http://www.pôle-géosciences.fr/index.htm' target='_blank'>Site Web</a></p>"
+           }]
+      }
+  ) ;
+
+
+  var infoDiv = document.getElementById("info");
+  infoDiv.innerHTML = "<p> SDK version " + Gp.sdkVersion + " (" + Gp.sdkDate + ")</p>";
+
+export { map };
