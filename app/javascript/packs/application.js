d38e47aa524c3ab55b305b6598c73aa5ab05b593
antonin cholet <antonin@cholet-avocat.fr>
2020-06-11 18:09:01 +0200

    add parcel selector on map

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -29,7 +29,6 @@ import "bootstrap";
 // import { initSelect2 } from '../components/init_select2';
 import { initMapbox } from '../plugins/init_mapbox';
 
-
 document.addEventListener('turbolinks:load', () => {
   // Call your functions here, e.g:
   // initSelect2();
